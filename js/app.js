5ec555a81c2d87a2df4f75900e73c9aa5850ecf7
justinpastine <justin.pastine@gmail.com>
2022-12-18 13:48:44 -0500

    add chip button align

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
 
 /*------------------- Variables --------------------------------*/
 let deckOfCards = []
-let playerTurn
-let dealerTurn
+let player
+let dealer
 let winner = false
 let bankRoll = 2000
 let cardValue = 0
@@ -43,3 +43,52 @@ playAgainBtn.addEventListener('click', function(evt){
 
 
 /*--------------------- Functions --------------------------------*/
+function startGame (){
+  //screen should be blank except for chips, "place your bet message", and deal button
+}
+
+function render () {
+  
+}
+
+function placeBet (){
+  //player clicks chips to place bet
+  //bet amount should reflect bank balance
+}
+
+function shuffleCards() {
+  //shuffle card array
+  //if card array reaches 0, cards should be reshuffled
+}
+
+function dealCards(){
+  //deal shuffled cards
+  //cards should be dealt in order, player gets one, dealer gets face down card, then player, then dealer face up
+  //deal button should disappear and hit and stand buttons should appear
+  //player and dealer cards should show the sum value of dealt cards
+  //initializes playerTurn 
+}
+
+function playerTurn() {
+//options to hit or stand
+//stand should end player turn
+//hit draws another card from deck and changes value of cards in player value
+//anytime total of cards is 21, player auto stands 
+//if players fisrt two cards equal 21, and dealers do not, player message should read Blackjack and player wins 
+//if sum value is over 21, player turn ends and message should read bust. dealer card should flip and and dealer message should read dealer wins
+}
+
+function dealerTurn() {
+  //face down card flips and dealer total updates
+  //if dealer cards are <= 16, dealer hits
+  //if dealer cards are >=17 and <=20, delaer stands
+  //if dealer has 21, dealer automatically stands 
+  //if dealer is over 21, message should read bust and player message should read player wins
+
+}
+
+function checkForWinner() {
+  //compare card totals and messages should change for win loss.
+  //bank balance reflects winnings if player wins and adjusts properly, 2:1 regular win, 3:2 blackjack win
+}
+
